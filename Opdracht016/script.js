3a2d6e73bf8b7f8b7e1ada245148f6cb2506e2a7
Elpida-web <84407349+Elpida-web@users.noreply.github.com>
2021-06-13 14:04:08 +0200

    Add files via upload

---
+++ Opdracht016/script.js
@@ -0,0 +1,9 @@
+const wallPaintColorAndSide = function (color, side) {
+    console.log("The "+ side, "wall has been painted "+ color);
+};
+
+const color = "red";
+const side = "north";
+
+wallPaintColorAndSide(color, side);
+//* Multiple arguments
